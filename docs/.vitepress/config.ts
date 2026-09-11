39f4ed554a500005cb26e21390e00ab9d0cf69da
mroczect <mroczect@proton.me>
2026-09-12 03:33:26 +0700

    corrtct brach name

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -202,7 +202,7 @@ export default withPwa(
         },
 
         editLink: {
-          pattern: `${REPO_URL}/edit/main/docs/:path`,
+          pattern: `${REPO_URL}/edit/master/docs/:path`,
           text: 'Edit this page on GitHub',
         },
 
