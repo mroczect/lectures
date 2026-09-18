2a897c92482b656da42f0fc84460caa92a6081e6
mroczect <mroczect@proton.me>
2026-09-19 00:36:36 +0700

    feat(config): add metadata configuration
    Define author, theme color, and description meta tags.

---
+++ config/metadata.ts
@@ -0,0 +1,11 @@
+import type * as Preset from '@docusaurus/preset-classic';
+
+export const metadata: Preset.ThemeConfig['metadata'] = [
+    { name: 'author', content: 'Muhammad Riduwan Khafidi' },
+    { name: 'theme-color', content: '#3e63dd' },
+    {
+        name: 'description',
+        content:
+            'Dokumentasi materi kuliah, tugas, dan catatan untuk mahasiswa Teknologi Rekayasa Perangkat Lunak Politeknik Negeri Batam.',
+    },
+];
